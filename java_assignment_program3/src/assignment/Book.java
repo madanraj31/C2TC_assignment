b84343515fe6d77ea6311a2937a7e77f5d240eaf
STUDENT <STUDENT@DESKTOP-V5KJQDL>
2025-08-21 11:58:02 -0500

    Assignment 3 programs

---
+++ java_assignment_program3/src/assignment/Book.java
@@ -0,0 +1,27 @@
+package assignment;
+
+public class Book extends Item{
+	private String author;
+
+	public Book(int id, String title, String author) {
+		super(id, title);
+		this.author = author;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [author=" + author + ",id="+id+",title="+title+"]";
+	}
+
+	
+
+}
+
